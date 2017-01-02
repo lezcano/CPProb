d3c371b305ee909b992803522ce73ccd90168843
lezkus <lezcano-93@hotmail.com>
2017-01-02 17:57:58 +0100

    .gitignore updated

--- cmake-build-debug/src/CMakeFiles/CPProb_lib.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libCPProb_lib.pdb"
-  "libCPProb_lib.a"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/CPProb_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- cmake-build-debug/src/CMakeFiles/CPProb_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libCPProb_lib.a"
-)
