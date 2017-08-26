8ab74c8e7bb8fd596ac0e6b646422afa2a72ee96
lezcano <lezcano-93@hotmail.com>
2017-08-26 19:20:12 +0100

    minor

--- include/models/models.hpp
+++ include/models/models.hpp
@@ -87,7 +87,6 @@ void normal_rejection_sampling(const RealType y1, const RealType y2)
     const RealType sigma = std::sqrt(2);
 
     const int max_it = 10000;
-    int it = 0;
 
     // Sample from Normal Distr
     const auto maxval = pdf(boost::math::normal_distribution<RealType>(mu_prior, sigma_prior), mu_prior);
@@ -96,7 +95,7 @@ void normal_rejection_sampling(const RealType y1, const RealType y2)
     RealType mu;
 
     cpprob::start_rejection_sampling();
-    while (it++ < max_it) {
+    while (true) {
         mu = cpprob::sample(proposal, true);
         if (cpprob::sample(accept) <
             pdf(boost::math::normal_distribution<RealType>(mu_prior, sigma_prior), mu)) {
@@ -110,26 +109,6 @@ void normal_rejection_sampling(const RealType y1, const RealType y2)
     cpprob::observe(likelihood, y2);
     cpprob::predict(mu, "Mu");
 }
-/*
-    Truth
-   [[ 0.3775 0.3092 0.3133]
-    [ 0.0416 0.4045 0.5539]
-    [ 0.0541 0.2552 0.6907]
-    [ 0.0455 0.2301 0.7244]
-    [ 0.1062 0.1217 0.7721]
-    [ 0.0714 0.1732 0.7554]
-    [ 0.9300 0.0001 0.0699]
-    [ 0.4577 0.0452 0.4971]
-    [ 0.0926 0.2169 0.6905]
-    [ 0.1014 0.1359 0.7626]
-    [ 0.0985 0.1575 0.744 ]
-    [ 0.1781 0.2198 0.6022]
-    [ 0.0000 0.9848 0.0152]
-    [ 0.1130 0.1674 0.7195]
-    [ 0.0557 0.1848 0.7595]
-    [ 0.2017 0.0472 0.7511]
-    [ 0.2545 0.0611 0.6844]]
-*/
 
 template<std::size_t N>
 void hmm(const std::array<double, N> & observed_states)
@@ -139,13 +118,13 @@ void hmm(const std::array<double, N> & observed_states)
     using boost::random::uniform_smallint;
 
     constexpr int k = 3;
-    static std::array<double, k> state_mean {{-1, 0, 1}};
-    static std::array<std::array<double, k>, k> T {{{{ 0.1,  0.5,  0.4 }},
-                                                    {{ 0.2,  0.2,  0.6 }},
-                                                    {{ 0.15, 0.15, 0.7 }}}};
+    static const std::array<double, k> state_mean {{-1, 0, 1}};
+    static const std::array<std::array<double, k>, k> T {{{{ 0.1,  0.5,  0.4 }},
+                                                          {{ 0.2,  0.2,  0.6 }},
+                                                          {{ 0.15, 0.15, 0.7 }}}};
     uniform_smallint<> prior {0, 2};
     auto state = cpprob::sample(prior, true);
-    cpprob::predict(state, "Initial state");
+    cpprob::predict(state, "State");
     for (const auto obs : observed_states) {
         discrete_distribution<> transition_distr {T[state].begin(), T[state].end()};
         state = cpprob::sample(transition_distr, true);
