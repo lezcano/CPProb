f7d82db363dcbd1d383aa9e98b51894819bf3a39
lezcano <lezcano-93@hotmail.com>
2017-09-04 23:20:45 +0100

    First try

--- dependencies/build/flatbuffers-prefix/src/flatbuffers-build/CMakeFiles/flathash.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for flathash.
-# This may be replaced when dependencies are built.
--- dependencies/build/flatbuffers-prefix/src/flatbuffers-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lezkus/Documents/CPProb/dependencies/build/flatbuffers-prefix/src/flatbuffers
-# Build directory: /home/lezkus/Documents/CPProb/dependencies/build/flatbuffers-prefix/src/flatbuffers-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(flattests "/home/lezkus/Documents/CPProb/dependencies/build/flatbuffers-prefix/src/flatbuffers-build/flattests")
