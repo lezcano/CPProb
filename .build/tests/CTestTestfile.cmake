a0c580f1fa487c54976a5e3638384ec33c18f713
lezcano <lezcano-93@hotmail.com>
2017-09-13 15:32:36 +0100

    travis

--- .build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lezkus/Documents/CPProb/tests
-# Build directory: /home/lezkus/Documents/CPProb/.build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("cpprob")
