c2b92d22cfe070526a91dc69a5bbafc714fdd380
lezcano <lezcano-93@hotmail.com>
2017-09-13 17:48:50 +0100

    Boost Static Libs

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -9,7 +9,7 @@ rm -rf "${CMAKE_INSTALL_PREFIX}" "${CMAKE_BINARY_DIR}"
 mkdir -p "${CMAKE_INSTALL_PREFIX}" "${CMAKE_BINARY_DIR}"
 (cd "${CMAKE_BINARY_DIR}" &&
   cmake "${CMAKE_SOURCE_DIR}" -DCMAKE_INSTALL_PREFIX="${CMAKE_INSTALL_PREFIX}" "${@}")
-cmake --build "${CMAKE_BINARY_DIR}"
+cmake --build "${CMAKE_BINARY_DIR}" -- -j4
 
 echo "************************************************************************"
 echo "When setting up CPProb, you should add '${CMAKE_INSTALL_PREFIX}' to 'CMAKE_PREFIX_PATH'"
