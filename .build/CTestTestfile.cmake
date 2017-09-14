35f66ce9d2fa42622e2360b5dfc332a16595d2f4
lezcano <lezcano-93@hotmail.com>
2017-09-14 02:48:31 +0100

    Include what you use

--- .build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lezkus/Documents/CPProb
-# Build directory: /home/lezkus/Documents/CPProb/.build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
